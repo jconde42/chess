9a537519dbc4b8825e4271493d06e7403bfcb552
Janner Conde <condejanner@gmail.com>
2015-12-02 20:54:59 -0700

    improved input

--- lib/chess.rb
+++ lib/chess.rb
@@ -17,30 +17,35 @@ def play
     puts " It is #{turn}'s turn"
     inputs = get_inputs
 
-    target_position = inputs[:target_position] 
-    piece = board.find(inputs[:piece_position])
+    if inputs
+      target_position = inputs[:target_position] 
+      piece = board.find(inputs[:piece_position])
 
-    # selected a black space to move?
-    if piece.nil?
-      puts "There is no piece at that position (Press Enter)"
-      gets
-      next
-    end
+      # selected a black space to move?
+      if piece.nil?
+        puts "There is no piece at that position (Press Enter)"
+        gets
+        next
+      end
 
-    # selected the other teams piece?
- #   if piece.team != turn
- #     puts "That's not your piece (Press Enter)"
- #     gets
- #     next
- #   end
+      # selected the other teams piece?
+      #   if piece.team != turn
+      #     puts "That's not your piece (Press Enter)"
+      #     gets
+      #     next
+      #   end
 
-    pre_move_position = piece.position
+      pre_move_position = piece.position
 
-    if piece.move(target_position: target_position, board: board)
+      if piece.move(target_position: target_position, board: board)
         board.move(piece_pre_position: pre_move_position,
                    piece_post: piece,
                    special: piece.special)
         turn = change_turn(turn)
+      end
+    else
+      puts "That not a valid move (Press Enter)"
+      gets
     end
 
   end
@@ -50,17 +55,32 @@ end
 def get_inputs
   puts "Enter a move: (mover, target)"
   raw = gets.chomp
-  mover = raw[/^../]
-  target = raw[/..$/]
+  mover = numerate(raw[/^../])
+  target = numerate(raw[/..$/])
 
-  return { piece_position: numerate(mover), target_position: numerate(target) }
+  if !valid_coordinate?(mover) || !valid_coordinate?(target)
+    return false
+  else
+    return { piece_position: mover, target_position: target }
+  end
 end
 
-def change_turn string
+def change_turn(string)
   if string == "white"
     return "black"
   else
     return "white"
+  end
+end
+
+def valid_coordinate?(arr)
+  x = arr[0]
+  y = arr[1]
+
+  if x < 0 or x > 7 or y < 0 or y > 7
+    return false
+  else
+    return true
   end
 end
 
